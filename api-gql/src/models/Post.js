3517a912909bb833d551baa198695c2cfb9fade3
Rayza Oliveira <rayza.ocr@gmail.com>
2020-11-22 19:16:57 -0300

    Criando modelos de Post e User

---
+++ api-gql/src/models/Post.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+export default mongoose.model("Post", Schema);
