4ca7d7d03a330b17ae5238e3ac6f81fd2483b4bd
Rayza Oliveira <rayza.ocr@gmail.com>
2020-11-22 19:15:54 -0300

    Alterando local do startServer

--- api-gql/startServer.js
+++
@@ -1,6 +0,0 @@
-import { ApolloServer } from "apollo-server";
-
-function startServer({ typeDefs, resolvers }) {
-  const server = new ApolloServer({ typeDefs, resolver });
-  server.listen().then(({ url }) => console.log(`🔥 Server started at ${url}`));
-}
